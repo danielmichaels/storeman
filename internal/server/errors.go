f2a82e87c0077d0d17c79aa5be1fe33efa441003
Daniel Michaels <dan@danielms.site>
2022-06-11 21:08:12 +1000

    Add an 404 page
    Signed-off-by: Daniel Michaels <dan@danielms.site>

--- internal/server/errors.go
+++ internal/server/errors.go
@@ -7,9 +7,8 @@ import (
 )
 
 func (app *Server) notFound(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(404)
-	w.Write([]byte("404 Not Found"))
-
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusNotFound, "404.tmpl", data)
 }
 func (app *Server) methodNotAllowed(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(405)
@@ -19,7 +18,8 @@ func (app *Server) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.Logger.Error().Err(err).Msgf(trace)
 
-	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	//http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	http.Error(w, err.Error(), http.StatusInternalServerError)
 }
 
 func (app *Server) clientError(w http.ResponseWriter, status int) {
